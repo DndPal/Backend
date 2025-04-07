a44b38d0b04fb1fc4def0e7b283fbc169a28a6d1
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-04-07 09:18:53 +0300

    Password hashing
    Unifinished iteration

--- dn-dhelper/src/authentication/services/register-user.service.ts
+++ dn-dhelper/src/authentication/services/register-user.service.ts
@@ -2,22 +2,35 @@ import { SaveUserUseCase } from "src/user/services/usecases/save-user.usecase";
 import { RegisterUserPort, RegisterUserUseCase } from "./usecases/register-user.usecase";
 import { FindByUsernameUseCase } from "src/user/services/usecases/find-by-username.usecase";
 import { BadRequestException } from "@nestjs/common";
+import { HashPasswordUseCase } from "./usecases/hash-password.usecase";
+import * as bcrypt from 'bcrypt';
 
 export class RegisterUserService implements RegisterUserUseCase {
     constructor(
         private readonly saveUserService: SaveUserUseCase,
-        private readonly findByUsername: FindByUsernameUseCase
+        private readonly findByUsernameService: FindByUsernameUseCase,
+        private readonly hashPasswordService: HashPasswordUseCase
     ) {}
 
     async execute(payload: RegisterUserPort): Promise<void> {
-        const { username } = payload;
-        const existingUser = await this.findByUsername.execute({ username: username });
+        const { username, password } = payload;
+        const existingUser = await this.findByUsernameService.execute({ username: username });
 
         if(existingUser) {
             throw new BadRequestException('Username already exists');
         }
 
+        const hashedPassword = await this.hashPasswordService.execute({ password: password });
+
+
+
         await this.saveUserService.execute(payload);
+    }
+
+    private async hashPassword(password: string): Promise<string> {
+        const saltOrRounds = 10;
+        const hash = await bcrypt.hash(password, saltOrRounds);
+        return hash;
     }
 
 
